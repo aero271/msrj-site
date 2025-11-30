9d96feac41b7ccadde85e6c7303c6817635fde52
aero271 <erenscar1@gmail.com>
2025-11-29 22:48:02 -0500

    Fixed sitemap

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,4 +1,4 @@
-import { pool } from '$lib/server/db';
+//import { pool } from '$lib/server/db';
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET() {
@@ -6,18 +6,31 @@ export async function GET() {
         '/about',
         '/articles',
         '/editorial',
-        '/issues'
+        '/issues',
+        '/articles/3',
+        '/articles/4',
+        '/articles/5',
+        '/articles/6',
+        '/articles/7',
+        '/articles/8',
+        '/articles/9',
+        '/articles/10',
     ];
     
+    /*
     const [articles] = await pool.query('SELECT * FROM articles;');
     for (let a of articles) {
         urls.push(`/articles/${a.id}`);
     }
+    */
     let xmlBody = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
     for (let url of urls) {
         xmlBody += `<url><loc>https://masteryjournal.org${url}</loc></url>`
     }
     xmlBody += '</urlset>'
 
-    return new Response(xmlBody, { headers: { 'Content-Type': 'application/xml' }});
+    return new Response(xmlBody, { 
+        status: 200,
+        headers: { 'Content-Type': 'application/xml' },
+    });
 };
